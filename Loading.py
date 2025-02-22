7b5975cb467d4b7f804bf02bb28e83d01eca3524
jacobszc <jacobszc@buffaol.edu>
2025-02-22 09:35:47 -0500

    added grass and did some scaling

--- Loading.py
+++ Loading.py
@@ -10,6 +10,16 @@ WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Loading Screen")
 
+
+## load images ########
+
+tree = pygame.image.load("images/tree.png")
+tree = pygame.transform.scale(tree, (250, 350))
+
+
+grass = pygame.image.load("images/grass.png")
+grass = pygame.transform.scale(grass, (900, 150))
+
 # Set up colors
 WHITE = (255, 255, 255)
 
@@ -21,6 +31,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    screen.blit(grass, (GRASS_LOC))
+    screen.blit(tree, (50, 250))
+    
+    
+    
+    
     
     pygame.display.flip()  # Update display
 
