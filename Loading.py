60b92a3e073e8c894c28ce303371e0eb7f57ff3e
jacobszc <jacobszc@buffaol.edu>
2025-02-22 18:37:41 -0500

    added star animation and loading title

--- Loading.py
+++ Loading.py
@@ -1,6 +1,6 @@
 import pygame
 import config
-from config import *
+
 
 # Initialize Pygame
 pygame.init()
@@ -10,15 +10,27 @@ WIDTH, HEIGHT = 800, 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Loading Screen")
 
+clock = pygame.time.Clock()
+
+TIMER_EVENT = pygame.USEREVENT + 1  
+pygame.time.set_timer(TIMER_EVENT, 300)
+
+
 
 ## load images ########
 
 tree = pygame.image.load("images/tree.png")
-tree = pygame.transform.scale(tree, (250, 350))
+tree = pygame.transform.scale(tree, config.TREE_SIZE )
 
 
 grass = pygame.image.load("images/grass.png")
-grass = pygame.transform.scale(grass, (900, 150))
+grass = pygame.transform.scale(grass, config.GRASS_SIZE )
+
+star = pygame.image.load("images/star.png")
+star = pygame.transform.scale(star, config.STARSIZE )
+
+loading = pygame.image.load("images/loading.png")
+loading = pygame.transform.scale(loading, config.LOADING_SIZE )
 
 # Set up colors
 WHITE = (255, 255, 255)
@@ -26,19 +38,27 @@ WHITE = (255, 255, 255)
 # Game loop
 running = True
 while running:
-    screen.fill(LIGHT_PURPLE)  # Fill screen with white
+    screen.fill(config.LIGHT_PURPLE)  # Fill screen with white
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    screen.blit(grass, (GRASS_LOC))
-    screen.blit(tree, (50, 250))
+    
+    
+        if event.type == TIMER_EVENT:
+             config.updateStar(config.DX,config.DY)
     
     
     
+    screen.blit(star, (config.X, config.Y))
+    screen.blit(grass, (config.GRASS_LOC))
+    screen.blit(tree, config.TREE_LOC)
+    screen.blit(loading, config.LOADING_LOC)
     
+    
+
     
     pygame.display.flip()  # Update display
-
+    clock.tick(30)
 # Quit Pygame
 pygame.quit()
--- config.py
+++ config.py
@@ -5,6 +5,52 @@ DARK_PURPLE = (150, 112, 150)
 BLACK = (0, 0, 0)
 GRAY = (180, 180, 180)
 
-
-
+## asset locations load screen ######
 GRASS_LOC = -35,450
+LOADING_LOC = 225, 50
+TREE_LOC = 50, 250
+
+
+TREE_SIZE = 250, 350
+GRASS_SIZE = 900, 150
+LOADING_SIZE = 350, 125
+
+### vars needed for moving star
+
+X ,Y = 100, 300
+DX, DY = 100, -50
+STARSIZE = (50,50)
+
+
+
+
+
+### animation fucntions ####
+
+
+
+
+
+def updateStar(dx,dy):
+    
+    global X, Y  # Access and modify global variables
+
+    if(X >=600):
+        X = 100
+        Y= 300
+    X += dx
+    Y += dy
+    return X, Y  # Return updated values
+
+    
+
+
+
+
+
+
+ 
+
+
+
+
