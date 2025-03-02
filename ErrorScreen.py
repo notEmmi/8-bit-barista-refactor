856e989d1922231631fcaa2a831268a9499d0189
Emrana Begum <emranabe@buffalo.edu>
2025-03-02 15:05:57 -0500

    update asset paths in ErrorScreen for consistency with new directory structure

--- ErrorScreen.py
+++ ErrorScreen.py
@@ -23,20 +23,20 @@ def runError():
     # Load font
     font = pygame.font.Font(None, FONT_SIZE)
 
-    darkbg = pygame.image.load("images/darkbg.png")
+    darkbg = pygame.image.load("assets/images/others/darkbg.png")
     darkbg = pygame.transform.scale(darkbg, (WIDTH, HEIGHT))
 
-    errorsign = pygame.image.load("images/errorsign_transparent.png")
+    errorsign = pygame.image.load("assets/images/others/errorsign_transparent.png")
     errorsign = pygame.transform.scale(errorsign, (250, 400))
 
-    cloud = pygame.image.load("images/rain_transparent.png")
+    cloud = pygame.image.load("assets/images/others/rain_transparent.png")
     cloud = pygame.transform.scale(cloud, (100, 100))
 
 
     # Error message
 
     mixer.init()
-    mixer.music.load("tracks/error.mp3")
+    mixer.music.load("assets/sounds/error.mp3")
     mixer.music.play()
 
 
@@ -53,7 +53,6 @@ def runError():
         screen.blit(cloud, (((WIDTH/2)+150),(HEIGHT/2)-250))
 
 
-    
 
         # Event handling
         for event in pygame.event.get():
