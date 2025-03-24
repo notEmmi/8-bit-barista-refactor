59ba3e86e91048eb561e0742b7a375b825ed9ed2
Emrana Begum <emranabe@buffalo.edu>
2025-03-24 13:38:44 -0400

    Add new tomato and wheat images, update paths in Plant_Objects tileset, and remove outdated images

--- assets/map/Plant_Objects.tsx
+++ assets/map/Plant_Objects.tsx
@@ -2,39 +2,39 @@
 <tileset version="1.10" tiledversion="1.11.2" name="Plant_Objects" tilewidth="16" tileheight="16" tilecount="12" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="./wheat1.png" width="16" height="16"/>
+    <image source="../images/plants/wheat1.png" width="16" height="16"/>
  </tile>
  <tile id="1">
-  <image source="./wheat2.png" width="16" height="16"/>
+    <image source="../images/plants/wheat2.png" width="16" height="16"/>
  </tile>
  <tile id="2">
-  <image source="./wheat3.png" width="16" height="16"/>
+    <image source="../images/plants/wheat3.png" width="16" height="16"/>
  </tile>
  <tile id="3">
-  <image source="./wheat4.png" width="16" height="16"/>
+    <image source="../images/plants/wheat4.png" width="16" height="16"/>
  </tile>
  <tile id="4">
-  <image source="./wheat5.png" width="16" height="16"/>
+    <image source="../images/plants/wheat5.png" width="16" height="16"/>
  </tile>
  <tile id="5">
-  <image source="./wheat6.png" width="16" height="16"/>
+    <image source="../images/plants/wheat6.png" width="16" height="16"/>
  </tile>
  <tile id="6">
-  <image source="./tomato1.png" width="16" height="16"/>
+    <image source="../images/plants/tomato1.png" width="16" height="16"/>
  </tile>
  <tile id="7">
-  <image source="./tomato2.png" width="16" height="16"/>
+    <image source="../images/plants/tomato2.png" width="16" height="16"/>
  </tile>
  <tile id="8">
-  <image source="./tomato3.png" width="16" height="16"/>
+    <image source="../images/plants/tomato3.png" width="16" height="16"/>
  </tile>
  <tile id="9">
-  <image source="./tomato4.png" width="16" height="16"/>
+    <image source="../images/plants/tomato4.png" width="16" height="16"/>
  </tile>
  <tile id="10">
-  <image source="./tomato5.png" width="16" height="16"/>
+    <image source="../images/plants/tomato5.png" width="16" height="16"/>
  </tile>
  <tile id="11">
-  <image source="./tomato6.png" width="16" height="16"/>
+    <image source="../images/plants/tomato6.png" width="16" height="16"/>
  </tile>
 </tileset>
