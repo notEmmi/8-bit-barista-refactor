716cd0e2de827b16e02194029a8b57686a0579d8
Emrana Begum <emranabe@buffalo.edu>
2025-03-24 12:56:52 -0400

    Update Plant_Objects tileset with new image paths and add missing tomato and wheat images

--- assets/map/Plant_Objects.tsx
+++ assets/map/Plant_Objects.tsx
@@ -2,39 +2,39 @@
 <tileset version="1.10" tiledversion="1.11.2" name="Plant_Objects" tilewidth="16" tileheight="16" tilecount="12" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="../../../../../../../Downloads/ezgif-split/tile000.png" width="16" height="16"/>
+  <image source="./wheat1.png" width="16" height="16"/>
  </tile>
  <tile id="1">
-  <image source="../../../../../../../Downloads/ezgif-split/tile001.png" width="16" height="16"/>
+  <image source="./wheat2.png" width="16" height="16"/>
  </tile>
  <tile id="2">
-  <image source="../../../../../../../Downloads/ezgif-split/tile002.png" width="16" height="16"/>
+  <image source="./wheat3.png" width="16" height="16"/>
  </tile>
  <tile id="3">
-  <image source="../../../../../../../Downloads/ezgif-split/tile003.png" width="16" height="16"/>
+  <image source="./wheat4.png" width="16" height="16"/>
  </tile>
  <tile id="4">
-  <image source="../../../../../../../Downloads/ezgif-split/tile004.png" width="16" height="16"/>
+  <image source="./wheat5.png" width="16" height="16"/>
  </tile>
  <tile id="5">
-  <image source="../../../../../../../Downloads/ezgif-split/tile005.png" width="16" height="16"/>
+  <image source="./wheat6.png" width="16" height="16"/>
  </tile>
  <tile id="6">
-  <image source="../../../../../../../Downloads/ezgif-split/tile006.png" width="16" height="16"/>
+  <image source="./tomato1.png" width="16" height="16"/>
  </tile>
  <tile id="7">
-  <image source="../../../../../../../Downloads/ezgif-split/tile007.png" width="16" height="16"/>
+  <image source="./tomato2.png" width="16" height="16"/>
  </tile>
  <tile id="8">
-  <image source="../../../../../../../Downloads/ezgif-split/tile008.png" width="16" height="16"/>
+  <image source="./tomato3.png" width="16" height="16"/>
  </tile>
  <tile id="9">
-  <image source="../../../../../../../Downloads/ezgif-split/tile009.png" width="16" height="16"/>
+  <image source="./tomato4.png" width="16" height="16"/>
  </tile>
  <tile id="10">
-  <image source="../../../../../../../Downloads/ezgif-split/tile010.png" width="16" height="16"/>
+  <image source="./tomato5.png" width="16" height="16"/>
  </tile>
  <tile id="11">
-  <image source="../../../../../../../Downloads/ezgif-split/tile011.png" width="16" height="16"/>
+  <image source="./tomato6.png" width="16" height="16"/>
  </tile>
 </tileset>
