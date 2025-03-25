d51f3811a7d54aca8a2fe7b0bea0f24858e458e9
Emrana Begum <emranabe@buffalo.edu>
2025-03-24 23:48:23 -0400

    add all tiles from tilesets to Gid_setter for easy access

--- assets/map/Plant_Objects.tsx
+++ assets/map/Plant_Objects.tsx
@@ -3,38 +3,98 @@
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
     <image source="../images/plants/wheat1.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="0"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="1"/>
+    </properties>
  </tile>
  <tile id="1">
     <image source="../images/plants/wheat2.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="1"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="2"/>
+    </properties>
  </tile>
  <tile id="2">
     <image source="../images/plants/wheat3.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="2"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="3"/>
+    </properties>
  </tile>
  <tile id="3">
     <image source="../images/plants/wheat4.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="3"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="4"/>
+    </properties>
  </tile>
  <tile id="4">
     <image source="../images/plants/wheat5.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="4"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="5"/>
+    </properties>
  </tile>
  <tile id="5">
     <image source="../images/plants/wheat6.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="5"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="0"/>
+    </properties>
  </tile>
  <tile id="6">
     <image source="../images/plants/tomato1.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="0"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="7"/>
+    </properties>
  </tile>
  <tile id="7">
     <image source="../images/plants/tomato2.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="1"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="8"/>
+    </properties>
  </tile>
  <tile id="8">
     <image source="../images/plants/tomato3.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="2"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="9"/>
+    </properties>
  </tile>
  <tile id="9">
     <image source="../images/plants/tomato4.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="3"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="10"/>
+    </properties>
  </tile>
  <tile id="10">
     <image source="../images/plants/tomato5.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="4"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="11"/>
+    </properties>
  </tile>
  <tile id="11">
     <image source="../images/plants/tomato6.png" width="16" height="16"/>
+    <properties>
+      <property name="growth_stage" type="int" value="5"/>
+      <property name="max_stage" type="int" value="5"/>
+      <property name="next_stage_gid" type="int" value="0"/>
+    </properties>
  </tile>
 </tileset>
