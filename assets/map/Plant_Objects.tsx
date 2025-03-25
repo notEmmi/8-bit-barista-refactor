3bbcaa0518dde6d27b8e5d6cb1165bd82ceeeb9c
Emrana Begum <emranabe@buffalo.edu>
2025-03-25 02:13:06 -0400

    Refactor plant growth propertiesto improve functionality and add harvest property for harvesting

--- assets/map/Plant_Objects.tsx
+++ assets/map/Plant_Objects.tsx
@@ -10,31 +10,32 @@
  <tile id="1">
   <image source="../images/plants/wheat2.png" width="16" height="16"/>
   <properties>
-    <property name="has_growth" type="bool" value="true"/>
+    <property name="growing" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="2">
   <image source="../images/plants/wheat3.png" width="16" height="16"/>
   <properties>
-    <property name="has_growth" type="bool" value="true"/>
+    <property name="growing" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="3">
   <image source="../images/plants/wheat4.png" width="16" height="16"/>
   <properties>
-    <property name="has_growth" type="bool" value="true"/>
+    <property name="growing" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="4">
   <image source="../images/plants/wheat5.png" width="16" height="16"/>
   <properties>
-    <property name="has_growth" type="bool" value="false"/>
+    <property name="growing" type="bool" value="true"/>
+    <property name="harvest" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="5">
   <image source="../images/plants/wheat6.png" width="16" height="16"/>
   <properties>
-    <property name="has_growth" type="bool" value="false"/>
+    <property name="growing" type="bool" value="false"/>
   </properties>
  </tile>
  <tile id="6">
@@ -46,31 +47,32 @@
  <tile id="7">
   <image source="../images/plants/tomato2.png" width="16" height="16"/>
   <properties>
-    <property name="has_growth" type="bool" value="true"/>
+    <property name="growing" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="8">
   <image source="../images/plants/tomato3.png" width="16" height="16"/>
   <properties>
-    <property name="has_growth" type="bool" value="true"/>
+    <property name="growing" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="9">
   <image source="../images/plants/tomato4.png" width="16" height="16"/>
   <properties>
-    <property name="has_growth" type="bool" value="false"/>
+    <property name="growing" type="bool" value="false"/>
   </properties>
  </tile>
  <tile id="10">
   <image source="../images/plants/tomato5.png" width="16" height="16"/>
   <properties>
-    <property name="has_growth" type="bool" value="false"/>
+    <property name="growing" type="bool" value="true"/>
+    <property name="harvest" type="bool" value="true"/>
   </properties>
  </tile>
  <tile id="11">
   <image source="../images/plants/tomato6.png" width="16" height="16"/>
   <properties>
-    <property name="has_growth" type="bool" value="false"/>
+    <property name="growing" type="bool" value="false"/>
   </properties>
  </tile>
 </tileset>
