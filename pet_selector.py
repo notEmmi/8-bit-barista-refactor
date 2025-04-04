521deb0bcc1b6766e77f1156999fd4ff97c4df32
jacobszc <jacobszc@buffaol.edu>
2025-04-03 21:40:09 -0400

    added dog images and functionality to scroll between them

--- pet_selector.py
+++ pet_selector.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 class PetSelector:
-    def __init__(self):
+    def __init__(self): ## pass a pet type that will be used to decide between lists of pngs to choose from
         # Initialize Pygame
         pygame.init()
 
@@ -9,7 +9,7 @@ class PetSelector:
         self.WIDTH, self.HEIGHT = 800, 600
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         pygame.display.set_caption("Pet Selector")
-
+        
         # Set up clock for frame rate
         self.clock = pygame.time.Clock()
         self.FPS = 60
@@ -19,14 +19,37 @@ class PetSelector:
         self.background = pygame.transform.scale(self.background, (self.WIDTH, self.HEIGHT))
 
 
-        self.rightarrowRect = pygame.Rect(self.WIDTH -100, self.HEIGHT/2, 50, 50)  # Placeholder for arrow rectangle
-        
+        self.rightarrowRect = pygame.Rect(self.WIDTH -150, self.HEIGHT/2, 50, 50)  # Placeholder for arrow rectangle
+        self.rightArrowsurface = pygame.Surface((self.rightarrowRect.width, self.rightarrowRect.height))
         self.rightarrow = pygame.image.load("images/rightarrow.png")
         self.rightarrow = pygame.transform.scale(self.rightarrow, (50, 50))
 
-        self.leftarrowRect = pygame.Rect( 100, self.HEIGHT/2, 50, 50)  # Placeholder for arrow rectangle
+        self.leftarrowRect = pygame.Rect((self.WIDTH-self.WIDTH)+100, self.HEIGHT/2, 50, 50)  # Placeholder for arrow rectangle
         self.leftarrow = pygame.image.load("images/leftarrow.png")
         self.leftarrow = pygame.transform.scale(self.leftarrow, (50, 50))
+        
+        
+        
+        
+        self.dogsList = ["assets/images/pets/greydog.png", "assets/images/pets/yellowdog.png", "assets/images/pets/browndog.png"]
+        self.dogRect = pygame.Rect(self.WIDTH/2 -70, self.HEIGHT/1.5, 150,150)
+        self.dog = pygame.image.load(self.dogsList[0])
+        self.dog = pygame.transform.scale(self.dog, (150,150))
+        self.arrowPressCount =0
+        
+    def parseDogListRight(self):
+        
+            self.dog = self.dogsList[self.arrowPressCount]
+            if self.arrowPressCount < 2:
+                self.arrowPressCount +=1
+                return
+
+    def parseDogListLeft(self):
+        
+            self.dog = self.dogsList[self.arrowPressCount]
+            if self.arrowPressCount <= 2 and self.arrowPressCount >=0 :
+                self.arrowPressCount -=1
+                return
         
 
     def run(self):
@@ -37,15 +60,29 @@ class PetSelector:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if self.rightarrowRect.collidepoint(event.pos):
+                       self.dog = self.parseDogListRight()
+                       self.dog = pygame.image.load(self.dogsList[self.arrowPressCount])
+                       self.dog = pygame.transform.scale(self.dog, (150,150))
+                    elif self.leftarrowRect.collidepoint(event.pos):
+                       self.dog = self.parseDogListLeft()
+                       self.dog = pygame.image.load(self.dogsList[self.arrowPressCount])
+                       self.dog = pygame.transform.scale(self.dog, (150,150))
+                     
 
             # Fill the screen with white (or any color you like)
             self.screen.fill((255, 255, 255))
             self.screen.blit(self.background, (0,0))
-            pygame.draw.rect(self.screen, self.WHITE, self.rightarrowRect)
+            self.screen.blit(self.rightArrowsurface, self.rightarrowRect.topleft)
             self.screen.blit(self.rightarrow, self.rightarrowRect.topleft) 
 
             pygame.draw.rect(self.screen, self.WHITE, self.leftarrowRect)
-            self.screen.blit(self.leftarrow, self.leftarrowRect.topleft) # RGB color
+            self.screen.blit(self.leftarrow, self.leftarrowRect.topleft)
+            
+            
+            self.screen.blit(self.dog, self.dogRect.topleft) # RGB color
 
             # Update the display
             pygame.display.flip()
